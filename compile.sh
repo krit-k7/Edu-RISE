9ab375ea5a1e5714d8afdb542cfce5e6e0255208
Krit Giri <kritgiri2006@gmail.com>
2026-09-10 23:52:01 +0530

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+# Add the local user binaries directory to PATH
 export PATH="/root/.local/bin:$PATH"
+
+# Set the NVM (Node Version Manager) directory
 export NVM_DIR="/root/.nvm"
+
+# Load NVM if the nvm.sh script exists
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
+# Navigate to the project directory
 cd "/mnt/d/risein/MOON/New folder"
+
+# Compile the project using Yarn
 yarn compile
